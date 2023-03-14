1fa88688948925fb9d587f66448050d8ff9522cf
Paulo Pereira <paulpere@uol.com.br>
2023-03-14 01:59:35 -0300

    Variação de condic c# para manhã_dia_noite com validade de numeros inválidos

---
+++ Logica_oper/Condit_1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Qual a hora");
+int hora=int.Parse(Console.ReadLine());
+
+if (hora >= 18 && hora<=24 )
+{
+    Console.WriteLine("Nesse horário " + hora.ToString()+ " horas dizemos 'Boa noite'");
+}
+
+else
+{
+    if (hora > 12 && hora < 18)
+    {
+        Console.WriteLine("Nesse horário " + hora.ToString() + " horas dizemos 'Boa tarde'");
+    }
+    else
+    {
+        if (hora >= 1 && hora <= 12)
+        {
+            Console.WriteLine("Nesse horário " + hora.ToString() + " horas dizemos 'Bom dia'");
+        }
+        else
+        {
+            Console.WriteLine("Nesse horário " + hora.ToString() + " horas dizemos que você digitou um horário inválido");
+        }
+    }
+
+}
+Console.ReadKey();
