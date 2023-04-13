e8a55617feae006ea3d32bc888f8dc54301431d7
Paulo Pereira <paulpere@uol.com.br>
2023-04-12 23:55:07 -0300

    Aula de operadores

--- Logica_oper/Condit_2/Program.cs
+++ Logica_oper/Condit_2/Program.cs
@@ -2,13 +2,12 @@
 
 double hora = double.Parse(Console.ReadLine());
 
-
 if (hora >= 0 && hora <= 12)
-    Console.WriteLine("dizemos que " + hora + " hora 'bom dia'");
+        Console.WriteLine("dizemos que " + hora + " hora 'bom dia'");
 else if (hora > 12 && hora <= 18)
     Console.WriteLine("dizemos que " + hora + " hora 'boa tarde'");
 else if (hora > 18 && hora <= 24)
-    Console.WriteLine("dizemos que " + hora + " hora 'boa noite'");
+        Console.WriteLine("dizemos que " + hora + " hora 'boa noite'");
 else
     Console.WriteLine("dizemos que você digitou um número inválido");
 
---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,54 @@
+﻿
+//FORMULÁRIO DE AUMENTO DE SALÁRIO COM BÔNUS E 13º
+
+/*double salario;
+double aumento = 1.10;
+double bonusJantarRomantico = 500;
+double antecipacaoFerias = 1.3;
+Console.WriteLine("Qual seu salário?");
+salario = double.Parse(Console.ReadLine());
+Console.WriteLine();
+salario = salario * aumento;
+Console.WriteLine("Seu salario com aumento é " + (salario));
+salario = salario + bonusJantarRomantico;
+Console.WriteLine("Seu salario com aumento e bonus é " + (salario));
+salario =salario * antecipacaoFerias;
+Console.WriteLine("Seu salario com aumento, bonus e antecipacao_férias é " + (salario));
+Console.ReadKey();*/
+
+
+//AUMENTO DE SALÁRIO POR SEXO, IDADE E NIVEL DE SALÁRIO
+
+/*
+Console.WriteLine("Idade/sexo/salário");
+int idade = int.Parse(Console.ReadLine());
+string sexo = Console.ReadLine().ToUpper();
+double salario = double.Parse(Console.ReadLine());
+Console.WriteLine("Suas \"info\" são "+idade+" "+sexo+" "+ salario);
+
+
+//Se for maior que 20 anos e for mulher OU salario menor que 100, salario deverá ter um aumento de 50%
+//Se for maior que 30 anos e for homem OU salario menor que 50, salário deverá ter um aumento de 10%
+
+if ((sexo == "M") && idade > 30 || salario < 50)
+{
+    salario = salario * 1.1;
+}
+if ((sexo == "F") && idade > 20 || salario < 100)
+{
+    salario = salario * 1.5;
+}
+Console.WriteLine(salario);
+Console.ReadKey();
+*/
+
+//INVERSÃO DA IDENTIFICAÇÃO DO SEXO
+
+Console.WriteLine("Digite o seu sexo");
+String sexo = Console.ReadLine().ToUpper();
+if (!(sexo =="M"))
+{ Console.WriteLine("Você é homem"); }
+
+
+
+
