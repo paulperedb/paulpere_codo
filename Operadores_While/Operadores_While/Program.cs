d52d0ca6d2c9ce02014413cf87f15f74dba6aa26
Paulo Pereira <paulpere@uol.com.br>
2023-04-23 16:42:49 -0300

    Exercícios de cond Switch e While

---
+++ Operadores_While/Operadores_While/Program.cs
@@ -0,0 +1,12 @@
+﻿//WHILE
+double numLido = -1;
+double soma = 0;
+
+while (numLido != 0)
+{
+    Console.Write("Digite um número ");
+    numLido = Double.Parse(Console.ReadLine());
+    soma += numLido;
+}
+Console.WriteLine("Total do números é " + soma);
+Console.ReadKey();
