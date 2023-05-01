f31878ba6a2916cd51a83db972d03755c878d5d1
Paulo Pereira <paulpere@uol.com.br>
2023-05-01 14:05:49 -0300

    Exercícios looping wihle_for

---
+++ ConsoleFor/ConsoleFor/Program.cs
@@ -0,0 +1,19 @@
+﻿int num1= int.Parse(Console.ReadLine());
+int num2=int.Parse(Console.ReadLine());
+int incremento = int.Parse(Console.ReadLine());
+int contador = 0;
+
+Console.WriteLine();
+
+Console.WriteLine("Contando em ordem crescente...");
+for (contador = num1; contador <= num2; contador+=incremento)
+{ Console.WriteLine(contador); }
+
+Console.WriteLine();
+
+Console.WriteLine("Contando em ordem decrescente...");
+for (contador = num2; contador > num1; contador -= incremento)
+{ Console.WriteLine(contador); }
+
+Console.ReadKey();
+
