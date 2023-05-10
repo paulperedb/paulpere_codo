75f73c3311f0b28e4dedb9a93a58eccfab1dd967
Paulo Pereira <paulpere@uol.com.br>
2023-05-10 17:16:39 -0300

    Exercício de mesa com vetor

---
+++ Vetor_mesa/Vetor_mesa/Program.cs
@@ -0,0 +1,17 @@
+﻿//Exercício de mesa com vetor
+int[] V = new int[5];
+int[] W = new int[5];
+
+int a = 2;
+int b = 5;
+
+Console.WriteLine("VetorV -->>VetorW");
+for (int d = 1; d < b; d++)
+{
+    V[d] = a;
+    a = a * 2;
+    W[d] = a;
+    Console.Write("[" + d + "] = " + V[d] + "-->> " + "[" + (d - 1) + "] = " + W[d]);
+    Console.WriteLine();
+}
+
